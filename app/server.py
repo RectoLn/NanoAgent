5f0413ccec1eb4eafc6b033622283c30e012830f
RectoLn <rectoln@outlook.com>
2026-04-22 23:26:18 +0800

    update port arguement

--- app/server.py
+++ app/server.py
@@ -443,7 +443,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "server:app",
         host="0.0.0.0",
-        port=9090,
+        port=int(os.getenv("APP_PORT", 9090)),
         reload=False,
         log_level="info",
     )
